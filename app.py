57d627506b6f287872a4b2b98db696404fbf4144
qaws7791 <qaws7791@gmail.com>
2023-05-18 14:02:22 +0900

    member 삭제 시 이미지 삭제하는 lambda함수 추가

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 # boto3
 
-lambda_clinet = boto3.client('lambda',
+lambda_client = boto3.client('lambda',
                              region_name = 'ap-northeast-2',
                              aws_access_key_id = aws_access_key_id,
                              aws_secret_access_key = aws_secret_access_key
@@ -163,6 +163,7 @@ def handle_member(id):
         password = request.form['password']
         member = member_col.find_one({'id':str(id)},{'_id':False})
         ph.verify(member['password'], str(password))
+        delete_image(str(id))
         member_col.delete_one({'id':member['id']})
         session.clear()
         return make_response(jsonify({'meg': 'success'}),200)
@@ -189,6 +190,10 @@ class ImageUploadError(Exception):
     def __init__(self):
         super().__init__('이미지 업로드에 실패했습니다.')
 
+class ImageDeleteError(Exception):    
+    def __init__(self):
+        super().__init__('이미지 삭제에 실패했습니다.')
+
 class InvalidExtensionError(Exception):    
   def __init__(self):
       super().__init__('잘못된 확장자입니다.')
@@ -206,20 +211,36 @@ def upload_image(id,image):
         if not is_allowed_file(filename):
           raise InvalidExtensionError
         payload = {
-        'id': id,
-        'extension': filename.rsplit('.', 1)[1],
-        'data': base64.b64encode(image.read()).decode('utf-8')
+          'id': id,
+          'extension': filename.rsplit('.', 1)[1],
+          'data': base64.b64encode(image.read()).decode('utf-8')
         }
-        response = lambda_clinet.invoke(
-        FunctionName='save-image-to-s3',
-        InvocationType='RequestResponse',
-        Payload= json.dumps(payload)
+        response = lambda_client.invoke(
+          FunctionName='save-image-to-s3',
+          InvocationType='RequestResponse',
+          Payload= json.dumps(payload)
         )
         response_payload = response['Payload'].read().decode('utf-8')
         response_payload = json.loads(response_payload)
         return response_payload['body']
     except Exception as e:
         raise ImageUploadError
+
+def delete_image(id):
+  try:
+      payload = {
+         'id': id
+      }
+      response = lambda_client.invoke(
+         FunctionName='delete-image-in-s3',
+         InvocationType='RequestResponse',
+         Payload= json.dumps(payload)
+      )
+      response_payload = response['Payload'].read().decode('utf-8')
+      response_payload = json.loads(response_payload)
+      return response_payload['body']
+  except Exception as e:
+      raise ImageDeleteError 
   
 ##### main #####
 if __name__ == '__main__':
